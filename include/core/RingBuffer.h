c168062740d6b0b22c32d5a857820395fd487aee
dust <dustlike@ms.simis.com.tw>
2019-04-01 17:38:25 +0800

    新增VariableRingBuffer RingBuffer的generic size版 原本的RingBuffer定義為64byte的VariableRingBuffer

--- include/core/RingBuffer.h
+++ include/core/RingBuffer.h
@@ -1,51 +1,103 @@
-/*
-  Copyright (c) 2014 Arduino.  All right reserved.
+#pragma once
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
 
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-  See the GNU Lesser General Public License for more details.
 
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
-#ifndef _RING_BUFFER_
-#define _RING_BUFFER_
-
+#include <stddef.h>
 #include <stdint.h>
 
-// Define constants and variables for buffering incoming serial data.  We're
-// using a ring buffer (I think), in which head is the index of the location
-// to which to write the next incoming character and tail is the index of the
-// location from which to read.
-#define SERIAL_BUFFER_SIZE 64
 
-class RingBuffer
+template<size_t BUFFER_SIZE_POWER>
+class VariableRingBuffer
 {
-  public:
-    uint8_t _aucBuffer[SERIAL_BUFFER_SIZE] ;
-    int _iHead ;
-    int _iTail ;
-
-  public:
-    RingBuffer( void ) ;
-    void store_char( uint8_t c ) ;
+	static constexpr size_t SERIAL_BUFFER_SIZE = 1 << BUFFER_SIZE_POWER;
+	
+public:
+	uint8_t _aucBuffer[SERIAL_BUFFER_SIZE] ;
+	volatile size_t _iHead ;
+	volatile size_t _iTail ;
+	
+	VariableRingBuffer() { clear(); }
+	void store_char(uint8_t);
 	void clear();
 	int read_char();
 	int available();
 	int availableForStore();
 	int peek();
 	bool isFull();
+	
+private:
+	size_t nextIndex(size_t index)
+	{
+		return (index + 1) & (SERIAL_BUFFER_SIZE - 1);
+	}
+};
 
-  private:
-	int nextIndex(int index);
-} ;
 
-#endif /* _RING_BUFFER_ */
+template<size_t X>
+void VariableRingBuffer<X>::store_char(uint8_t c)
+{
+	size_t i = nextIndex(_iHead);
+	
+	if ( i != _iTail )
+	{
+		_aucBuffer[_iHead] = c;
+		_iHead = i;
+	}
+}
+
+
+template<size_t X>
+void VariableRingBuffer<X>::clear()
+{
+	_iHead = _iTail = 0;
+}
+
+
+template<size_t X>
+int VariableRingBuffer<X>::read_char()
+{
+	if(_iTail == _iHead) return -1;
+	
+	uint8_t value = _aucBuffer[_iTail];
+	_iTail = nextIndex(_iTail);
+	
+	return value;
+}
+
+
+template<size_t X>
+int VariableRingBuffer<X>::available()
+{
+	return (SERIAL_BUFFER_SIZE + _iHead - _iTail) & (SERIAL_BUFFER_SIZE - 1);
+}
+
+
+template<size_t X>
+int VariableRingBuffer<X>::availableForStore()
+{
+	return (SERIAL_BUFFER_SIZE - 1 - _iHead + _iTail) & (SERIAL_BUFFER_SIZE - 1);
+}
+
+
+template<size_t X>
+int VariableRingBuffer<X>::peek()
+{
+	return (_iTail == _iHead)? -1 : _aucBuffer[_iTail];
+}
+
+
+template<size_t X>
+bool VariableRingBuffer<X>::isFull()
+{
+	return nextIndex(_iHead) == _iTail;
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+
+
+//64byte version of variable ring buffer.
+class RingBuffer : public VariableRingBuffer<6>
+{
+	//empty body. All methods inherit from VariableRingBuffer
+};
